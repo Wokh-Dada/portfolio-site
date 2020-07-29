ae25ffb5ba1bf1d19c492ec5b21b44323a739d85
Wokh-Dada <a.gezikhanov@mail.ru>
2020-07-29 11:01:09 +0300

    added svg to prepare for animation

--- src/components/cnt-flexy-view-abdullakh-promo-header-wrapper/res/view/cnt-flexy-view-abdullakh-promo-header-presentation/cnt-flexy-view-abdullakh-promo-header-presentation.tsx
+++ src/components/cnt-flexy-view-abdullakh-promo-header-wrapper/res/view/cnt-flexy-view-abdullakh-promo-header-presentation/cnt-flexy-view-abdullakh-promo-header-presentation.tsx
@@ -1,5 +1,6 @@
 import {Component, ComponentInterface, Event, EventEmitter, h, Prop, State} from '@stencil/core';
 import {AbdullakhHeaderPresentation} from "./interface/common.interface";
+import {StorageAbdullakhPromo} from "../../../../../utils/utils";
 
 @Component({
   tag: 'cnt-flexy-view-abdullakh-promo-header-presentation',
@@ -31,8 +32,147 @@ export class CntFlexyViewAbdullakhPromoHeaderPresentation implements ComponentIn
   render() {
     return (
       <header class="header_backg_black" id="header_backg_black" ref={(el) => this.headClass = el}>
-        <div  class="logoBack" innerHTML={this.categories.logoBack}>
-        {/*logo background*/}
+        <div class="svgMouseEffect">
+          <div class="svgGoLeft">
+            {/**/}
+            <span style={{left: '44%', top: '18%', transform: 'rotate(180deg)'}} class="yellowTriangle">
+              <svg id="triangle" width="25px" height="25px">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     width="25.675"
+                     height="22.236"
+                     viewBox="0 0 25.675 22.236"
+                >
+                  <path fill="none"
+                        stroke="#ff0"
+                        stroke-width="3"
+                        stroke-miterlimit="10"
+                        d="M12.837 3l5.12 8.868 5.12 8.868H2.597l5.12-8.868z"
+                  />
+                </svg>
+              </svg>
+            </span>
+            {/*  */}
+            <span style={{left: '70%', top: '20%'}} class="OrangeTriangle">
+              <svg id="triangle" width="25px" height="25px">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     width="25.675"
+                     height="22.236"
+                     viewBox="0 0 25.675 22.236"
+                >
+                  <path fill="none"
+                        stroke="#f06449"
+                        stroke-width="3"
+                        stroke-miterlimit="10"
+                        d="M12.837 3l5.12 8.868 5.12 8.868H2.597l5.12-8.868z"
+                  />
+                </svg>
+              </svg>
+            </span>
+            {/*  */}
+            <span style={{left: '0%', top: '80%', transform: 'rotate(45deg)'}} class="AquaSemiCircle">
+              <svg id="semiCircle">
+                <path fill="none"
+                      stroke="aqua"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M1.5 14.546C1.5 7.34 7.34 1.5 14.547 1.5c7.205 0 13.046 5.84 13.046 13.046"
+                />
+              </svg>
+            </span>
+            {/**/}
+            <span style={{left: '90%', top: '70%'}} class="YellowSemiCircle">
+              <svg id="semiCircle">
+                <path fill="none"
+                      stroke="#ff0"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M1.5 14.546C1.5 7.34 7.34 1.5 14.547 1.5c7.205 0 13.046 5.84 13.046 13.046"
+                />
+              </svg>
+            </span>
+            {/**/}
+            <span style={{left: '50%', bottom: '-20%', transform: 'rotate(45deg)'}} class="OrangeSpring">
+              <svg id="spring">
+                <path fill="none"
+                      stroke="#f06449"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M9.945 34.614S1.46 31.38 1.5 23.92s11.73-4.797 11.73-4.797S3.82 16.708 4.18 8.77s12.784-7.255 12.784-7.255"
+                />
+              </svg>
+            </span>
+            {/**/}
+            <span style={{left: '85%', bottom: '-12%'}} class="AquaLineBottom">
+              <svg id="line">
+                <path fill="none"
+                      stroke="aqua"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M4.92 2.514L21.964 19.56">
+
+                </path>
+              </svg>
+            </span>
+            {/*  */}
+            <span style={{left: '20%', top: '10%', transform: 'rotate(65deg)'}} class="AquaLineTop">
+              <svg id="line">
+                <path fill="none"
+                      stroke="aqua"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M4.92 2.514L21.964 19.56">
+
+                </path>
+              </svg>
+            </span>
+            {/*  */}
+          </div>
+          <div class="svgGoRight">
+            {/**/}
+            <span style={{left: '98%', top: '20%', transform: 'rotate(45deg)'}} class="AquaTriangle">
+              <svg id="triangle" width="25px" height="25px">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     width="25.675"
+                     height="22.236"
+                     viewBox="0 0 25.675 22.236"
+                >
+                  <path fill="none"
+                        stroke="aqua"
+                        stroke-width="3"
+                        stroke-miterlimit="10"
+                        d="M12.837 3l5.12 8.868 5.12 8.868H2.597l5.12-8.868z"
+                  />
+                </svg>
+              </svg>
+            </span>
+            {/*  */}
+            <span style={{left: '90%', top: '70%', transform: 'rotate(180deg)'}} class="AquaSemiCircle">
+              <svg id="semiCircle">
+                <path fill="none"
+                      stroke="aqua"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M1.5 14.546C1.5 7.34 7.34 1.5 14.547 1.5c7.205 0 13.046 5.84 13.046 13.046"
+                />
+              </svg>
+            </span>
+            {/**/}
+            <span style={{left: '10%', bottom: '-15%', transform: 'rotate(85deg)'}} class="YellowLineBottom">
+              <svg id="line">
+                <path fill="none"
+                      stroke="#ff0"
+                      stroke-width="3"
+                      stroke-miterlimit="10"
+                      d="M4.92 2.514L21.964 19.56">
+
+                </path>
+              </svg>
+            </span>
+            {/*  */}
+          </div>
+        </div>
+        <div class="logoBack" innerHTML={this.categories.logoBack}>
+          {/*logo background*/}
         </div>
         <div class="icons_block">
           <Social arr={this.categories.socialIcon}/>
